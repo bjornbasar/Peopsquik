cf23d67f36b9968acbd78be8b6036c2e1f3ad406
Bjorn Christian Basar <bjorn.basar@rippledev.com>
2011-09-13 17:10:11 +0800

    new

--- apps/controllers/actions/install.action.php
+++ apps/controllers/actions/install.action.php
@@ -21,7 +21,25 @@ fwrite($fh, $content);
 
 fclose($fh);
 
-mysql_connect($dbhost, $dbuser, $dbpass);
+mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
 mysql_select_db($dbname);
 
-$fh = fopen(APP_LIB . 'istrbuddy.sql', $mode);
+$fh = fopen(APP_LIB . 'istrbuddy.sql', 'r');
+
+$content = fread($fh, filesize(APP_LIB . 'istrbuddy.sql'));
+
+$queries = explode(';', trim($content, " ;"));
+array_pop($queries);
+
+foreach ($queries as $query)
+{
+	$query = trim($query);
+	mysql_query($query) or die(mysql_error());
+}
+
+echo 'Insert into users values (\'\', \'1\', \'' . $admin . '\', \'' . $adminpassword . '\', \'Administrator\', \'System Administrator User\' )';
+
+mysql_query('Insert into users values (\'\', \'1\', \'' . $admin . '\', \'' . $adminpassword . '\', \'Administrator\', \'System Administrator User\', 1 )') or die(mysql_error());
+
+$this->forward('sysinstall');
+
